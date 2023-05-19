17188387e920284046ec865ff499922ec33e7898
saikiran <saikiran.davangams@gmail.com>
2023-05-19 19:47:30 +0200

    files updated

--- src/utils.py
+++ src/utils.py
@@ -25,3 +25,35 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
+    try:
+        #X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            
+            model.fit(X_train,y_train)   # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train,y_train_pred)
+
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
+    except Exception as e:
+        raise CustomException(e,sys)
